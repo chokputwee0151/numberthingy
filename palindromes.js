cc8be8d48a6e70c726aba01b98a257e5057d5cd9
chokputwee0151 <113009253+chokputwee0151@users.noreply.github.com>
2024-01-25 16:43:37 +0700

    Add files via upload

---
+++ palindromes.js
@@ -0,0 +1,11 @@
+let palin = "radar"
+
+function strtoarr(x){
+    let arrayPalin = x.split('').toString()
+    let reversedArray = x.split('').reverse().toString()
+    console.log(reversedArray)
+    console.log(arrayPalin)
+    return Boolean(arrayPalin === reversedArray)
+}
+    
+console.log(strtoarr(palin))
