fad4679d8dd23a102753ce7b4bf1dcde2c0684d5
chokputwee0151 <113009253+chokputwee0151@users.noreply.github.com>
2024-01-25 16:55:44 +0700

    Update palindromes.js

--- palindromes.js
+++ palindromes.js
@@ -21,3 +21,13 @@ function strtoarr(x){
 }
     
 console.log(strtoarr(palin))
+----------------------------------------------------------------
+function isPalindrom(word) {
+  const chars = [...word] //string to array with spread [...]
+  console.log(chars)
+  for (let i = 0, j = chars.length - 1; j >= i; i++, j--) {
+    if (chars[i] !== chars[j]) return false
+  }
+  return true
+}
+console.log(isPalindrom('apple'))
